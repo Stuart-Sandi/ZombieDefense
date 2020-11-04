11375c5d243a270cf1e015b30d915ca3bb8f02c1
Stuart-Sandi <stuartsandi43@gmail.com>
2020-11-04 15:49:32 -0600

    Se crea el tablero de botones en la GUI

--- src/main/java/Controlador/ControladorZombieDefense.java
+++ src/main/java/Controlador/ControladorZombieDefense.java
@@ -2,10 +2,14 @@ package Controlador;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JButton;
 
 import Vista.VentanaInicio;
 
-public class ControladorZombieDefense implements ActionListener {
+public class ControladorZombieDefense implements ActionListener, MouseListener {
 
 	//VARIABLES
 	private VentanaInicio vInicio;
@@ -21,6 +25,42 @@ public class ControladorZombieDefense implements ActionListener {
 		switch (arg0.getActionCommand()) {
 			
 		}
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		
+		JButton botonTemp1 = (JButton)arg0.getComponent();
+        String identificadorBoton = botonTemp1.getActionCommand();
+        int x = Integer.parseInt(identificadorBoton.substring(0,identificadorBoton.indexOf(",")));
+        int y = Integer.parseInt(identificadorBoton.substring(1+identificadorBoton.indexOf(",")));
+        this.vInicio.tablero[x][y].setEnabled(false);
+        System.out.println("X: "+x+"  /  Y:"+y);
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
 		
 	}
 
--- src/main/java/Vista/VentanaInicio.java
+++ src/main/java/Vista/VentanaInicio.java
@@ -1,14 +1,22 @@
 package Vista;
 
-import java.awt.EventQueue;
-
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 import Controlador.ControladorZombieDefense;
+import javax.swing.JPanel;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Image;
 
 public class VentanaInicio {
 
+	public JButton[][] tablero;
 	private JFrame frame;
+	public JPanel panel, panel2;
+	public ImageIcon cesped;
 	
 	private ControladorZombieDefense controlador;
 
@@ -28,12 +36,71 @@ public class VentanaInicio {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 600, 500);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		//JFRAME
+		frame = new JFrame();
+		frame.setBounds(100, 100, 980, 700);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.getContentPane().setLayout(null);
+		frame.setLocationRelativeTo(null);
+		frame.setTitle("Zombie Defense");
+		
+		//JPANEL
+		panel = new JPanel();
+		panel.setBackground(Color.BLACK);
+		panel.setBounds(10, 50, 600, 600 );
+        panel.setLayout(new javax.swing.GroupLayout(panel));
+		frame.getContentPane().add(panel);
+		
+		panel2 = new JPanel();
+		panel2.setBackground(Color.BLUE);
+		panel2.setBounds(625, 50, 325, 600);
+		frame.getContentPane().add(panel2);
+		
+		//JLabel
+		JLabel lblNewLabel = new JLabel("TABLERO");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 20));
+		lblNewLabel.setBounds(10, 11, 109, 38);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblOpciones = new JLabel("OPCIONES");
+		lblOpciones.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 20));
+		lblOpciones.setBounds(625, 11, 109, 38);
+		frame.getContentPane().add(lblOpciones);
+		
+		//ImageIcon
+        this.cesped = new ImageIcon((new ImageIcon(getClass().getResource("/Imagenes/cesped.jpg")))
+        		.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT));
+		
+		
+		this.tablero = new JButton[20][20];
+		this.generarBotones();
 		frame.setVisible(true);
 	}
+	
+	
+	private void generarBotones(){
+        
+        for (int i = 0; i <20; i++) {
+            
+            for (int j = 0; j < 20; j++) {
+                
+                this.tablero[i][j] = new JButton(this.cesped);
+                this.tablero[i][j].setOpaque(true);
+                this.panel.add(this.tablero[i][j]);
+                this.tablero[i][j].setBounds(30*i, 30*j, 30, 30);
+                this.tablero[i][j].setActionCommand(i+","+j);//i+","+j
+                this.tablero[i][j].addMouseListener(controlador);
+
+            }
+        }
+    }
+	
+	private void generarBase() {
+		
+	}
+    
 	
 	private void setActionListener() {
 	/*
