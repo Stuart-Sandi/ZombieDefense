f3a23ff292c1b4a2ed3411063c94afea35ebffef
AndrewGutierrezCastro <andrewgc@estudiantec.cr>
2020-11-04 23:06:33 -0600

    Datos de cada tipo de personaje

--- src/main/java/modelo/Personaje.java
+++ src/main/java/modelo/Personaje.java
@@ -8,19 +8,36 @@ public class Personaje {
 	public HashMap<Integer, Item> inventario;
 	public Posicion posicion;
 	public TipoPersonaje tipo;
-	public int pasos, distanciaVision, vida, cantidadAcciones;
-	
+	public int pasos, distanciaVision, vida, cantidadAcciones, nivel, experiencia;
+	public Boolean vivo;
 			   
 	public Personaje(TipoPersonaje pTipo) {
 		super();
 		this.tipo = pTipo;
 		this.posicion = new Posicion();
+		
 	}
 	
 	public Personaje(TipoPersonaje pTipo, int x, int y) {
 		super();
 		this.tipo = pTipo;
 		this.posicion = new Posicion(x, y);
+		this.vida = pTipo.vida;
+		this.cantidadAcciones = pTipo.cantidadAcciones;
+		this.pasos = pTipo.pasos;
+		this.cantidadAcciones = pTipo.cantidadAcciones;
+		this.distanciaVision = pTipo.distanciaVision;
+		this.experiencia = pTipo.experiencia;
 	}
 	
+	public void Atacar(Posicion pPosicion, Personaje pPersonaje) {
+		//Todo hacer el metodo de atacar
+	}
+	
+	public void recibirAtaque(int valor) {
+		this.vida -= valor;
+		if (vida < 0) {
+			this.vivo = false;
+		}
+	}
 }
---
+++ src/main/java/modelo/ValoresDefecto.java
@@ -0,0 +1,10 @@
+package modelo;
+
+public interface ValoresDefecto {
+	public int vida = 100,
+			   pasos = 20, 
+			   distanciaVision = 5, 
+			   cantidadAcciones = 3, 
+			   nivel = 1, 
+			   experiencia = 0;
+}
